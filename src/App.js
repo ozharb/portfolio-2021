787d4ac24353dfff2f6a20f5fa5216d046247c32
Ozzie Harb <ozharb@gmail.com>
2021-03-28 17:58:02 -0700

    background and heading

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import "./App.css";
 
 function App() {
   const [offsetY, setOffsetY] = useState(0);
+  const [fade, setOpacity] = useState(0);
+  const [moveName, setOffsetYName] = useState(0);
+  
   const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  const handleScrollOpacity = () => setOpacity(window.pageYOffset);
+const handleName = () => window.pageYOffset>=300?setOffsetYName(window.pageYOffset-300):setOffsetYName(0);
+
+
+
+
+useEffect(()=>{
+  window.addEventListener("scroll", handleName);
+
+  return () => window.removeEventListener("scroll", handleName);
+}, []);
+
+  useEffect(()=>{
+    window.addEventListener("scroll", handleScrollOpacity);
+
+    return () => window.removeEventListener("scroll", handleScrollOpacity);
+  }, []);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -13,14 +34,41 @@ function App() {
 
   const renderContent = () => (
     <>
-      <div className="Parallax__content__heading">
-        <h1 className="Parallax__content__heading__text">Closure</h1>
-        <h2 className="Parallax__content__heading__caption">
-          Your one-stop source of Web Development tricks
-        </h2>
+   
+        <h1 className="header"
+        style={{opacity: `${1- fade *.005}`}}
+        // style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
+        >
+          <p style={{ transform: `translateY(-${offsetY * 0.5}px)` }}>H</p>
+          <p style={{ transform: `translateY(-${offsetY * 0.2}px)` }}>e</p>
+          <p style={{ transform: `translateY(${offsetY * 0.2}px)` }}>l</p>
+          <p style={{ transform: `translateY(${offsetY * 0.3}px)` }}>l</p>
+          <p style={{ transform: `translateY(${offsetY * 0.5}px)` }} >o</p>
+        </h1>
+
+      <div className="box b1">
+        <span className="b1-content">
+      <h2 
+      style={{ transform: `translateX(-${moveName *.2}px)` }}>
+        I'm Oz</h2>
+        <p style={{ transform: `translateX(${moveName *0.2}px)` }}>
+          Nice to meet you.
+        </p>
+        </span>
       </div>
-      <h3>Parallaxing</h3>
-      <div className="Parallax__content__cta">
+      <div className="box b2">
+        <p>
+          About Me
+        </p>
+        <p>
+          Projects
+        </p>
+        <p>
+         Contact
+        </p>
+      </div>
+      <div className="box b3">
+        <h2>Box 3</h2>
         <p>
           <b>1. Like the video.</b> Because it helps me and my channel
         </p>
@@ -31,18 +79,8 @@ function App() {
           <b>3. Follow the Github link.</b> And play with this code yourself!
         </p>
       </div>
-      <div className="Parallax__content__cta">
-        <p>
-          <b>1. Like the video.</b> Because it helps me and my channel
-        </p>
-        <p>
-          <b>2. Like the video.</b> To see more content like that!
-        </p>
-        <p>
-          <b>3. Follow the Github link.</b> And play with this code yourself!
-        </p>
-      </div>
-      <div className="Parallax__content__cta">
+      <div className="box b4">
+        <h2>Box 4</h2>
         <p>
           <b>1. Like the video.</b> Because it helps me and my channel
         </p>
@@ -57,21 +95,43 @@ function App() {
   );
 
   return (
-    <section className="Parallax">
+    <section className="App">
 
       <div
         className="Parallax__background"
         style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
       />
-      <div
+      {/* <div
         className="Parallax__background-triangles"
       style={{ transform: `translateY(-${offsetY * 0.7}px)` }}
+      /> */}
+      {/* <section className='images-background'>
+       <div
+        className="skyline sky-one"
+      style={{ transform: `translateY(-${offsetY * 0.7}px)` }}
       />
-      <div className='my-test'
+        <div
+        className="skyline sky-two"
+      style={{ transform: `translateY(-${offsetY * 0.7}px)` }}
+      />
+            <div
+        className="skyline sky-three"
+      style={{ transform: `translateY(-${offsetY * 0.7}px)` }}
+      />
+            <div
+        className="skyline sky-four"
+      style={{ transform: `translateY(-${offsetY * 0.7}px)` }}
+      />
+            <div
+        className="skyline sky-five"
+      style={{ transform: `translateY(-${offsetY * 0.7}px)` }}
+      />
+      
+      </section> */}
+      <div className='circle-1'
       style={{ transform: `translateY(-${offsetY * 0.4}px)` }}> 
-        <h4>parallax!</h4>
       </div>
-      <div className="Parallax__content">{renderContent()}</div>
+      <div className="main__content">{renderContent()}</div>
 
     </section>
   );
