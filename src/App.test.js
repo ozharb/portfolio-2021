4754507e488996f1301b1e059b0a139c795aaf75
Ozzie Harb <ozharb@gmail.com>
2021-04-02 14:45:23 -0700

    updated readme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders word', () => {
-  render(<App />);
-  const word = screen.getByText(/parallax!/i);
-  expect(word).toBeInTheDocument();
+window.matchMedia = window.matchMedia || function() {
+  return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {}
+  };
+};
+
+describe("Test", () => {
+  beforeAll(() => {
+    window.matchMedia = (query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })
+  });
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+    <App  />
+    , div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
